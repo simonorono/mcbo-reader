81c5a2f851dbd2cb977fbcff9135910f90e94827
Simón Oroño <simonorono@protonmail.com>
2024-04-19 11:39:38 -0400

    Add padding to avoid article content being too close to border

--- resources/views/show-feed-item.blade.php
+++ resources/views/show-feed-item.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <article class="prose mx-auto">
+    <article class="prose mx-auto px-4">
         {!! $item->content !!}
     </article>
 @endsection
